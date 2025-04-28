324760f59cb5e4cf0f79e09b6bf2af072b8d09d0
Jason Trigg <jasontrigg0@gmail.com>
2025-04-27 22:54:42 -0400

    2025-04-25 update

---
+++ src/run.sh
@@ -0,0 +1,13 @@
+#news
+cd news
+python generate_news.py
+#cd ../../web-app
+#npm run build
+#npm run deploy
+cd ..
+
+#approval ratings
+cd approval_ratings
+curl https://static.dwcdn.net/data/vknzT.csv?v=0 > polls.csv
+python approval_polling.py
+cd ..
