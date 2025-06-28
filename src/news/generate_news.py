10f663cacf7d32b62915d53b83ae656c4078941c
Jason Trigg <jasontrigg0@gmail.com>
2025-06-28 12:32:37 -0400

    2025-06-28 update

--- src/news/generate_news.py
+++ src/news/generate_news.py
@@ -12,7 +12,7 @@ import math
 def run_prompt_gemini(prompt):
     url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={os.environ['GEMINI_API_KEY']}"
     url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-thinking-exp:generateContent?key={os.environ['GEMINI_API_KEY']}"
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro-exp-03-25:generateContent?key={os.environ['GEMINI_API_KEY']}"
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro:generateContent?key={os.environ['GEMINI_API_KEY']}"
     headers = {
         "Content-Type": "application/json"
     }
