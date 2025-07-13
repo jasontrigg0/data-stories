6e91c3f603d678faea8b9dc6288a173bc65def30
Jason Trigg <jasontrigg0@gmail.com>
2025-07-13 01:05:22 -0400

    Minor run.sh tweak

--- src/run.sh
+++ src/run.sh
@@ -1,18 +1,19 @@
 #news
 cd news
-python generate_news.py
-#cd ../../web-app
-#npm run build
-#npm run deploy
+python3 generate_news.py
 cd ..
 
 #approval ratings
 cd approval_ratings
 curl https://static.dwcdn.net/data/vknzT.csv?v=0 > polls.csv
-python approval_polling.py
+python3 approval_polling.py
 cd ..
 
 #sports elos
 cd sports_elos
-python populate_data.py
+python3 populate_data.py
 cd ..
+
+#cd ../../web-app
+#npm run build
+#npm run deploy
