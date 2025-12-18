5018965f51bbc033bb23081a86a6212c0922fa86
Jason Trigg <jasontrigg0@gmail.com>
2025-12-18 06:56:39 -0500

    Drop chatbot arena history

--- web-app/src/AILeaderboard.tsx
+++ web-app/src/AILeaderboard.tsx
@@ -209,10 +209,6 @@ const tabs = [
     title: "Price vs Performance",
     content: (<PricePerformanceChart/>)
   },
-  {
-    title: "Chatbot Arena History",
-    content: (<iframe src='https://flo.uri.sh/visualisation/22378491/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style={{width:"100%",height:"600px"}} sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe>)
-  }
 ];
 
 export default AILeaderboard;
