d8edda7723675960fe6c73943fb8583055a82900
Jason Trigg <jasontrigg0@gmail.com>
2025-12-18 06:52:49 -0500

    2025-12-18 update

--- src/news/generate_news.py
+++ src/news/generate_news.py
@@ -14,8 +14,8 @@ import zlib
 def run_prompt_gemini(prompt):
     url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={os.environ['GEMINI_API_KEY']}"
     url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-thinking-exp:generateContent?key={os.environ['GEMINI_API_KEY']}"
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro:generateContent?key={os.environ['GEMINI_API_KEY']}"
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={os.environ['GEMINI_API_KEY']}"
+    #url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro:generateContent?key={os.environ['GEMINI_API_KEY']}"
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key={os.environ['GEMINI_API_KEY']}"
     headers = {
         "Content-Type": "application/json"
     }
